8f09e76f3d5f6b556798fafed2c507863ed922ae
Bawaskar Rahul <38488318+bawaskarrahul@users.noreply.github.com>
2018-09-11 17:16:11 +0530

    added java files
    added java files

---
+++ app/MyApp.java
@@ -0,0 +1,13 @@
+package com.TSI.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage("Hello World!");
+    }
+}
---
+++ app/Util.java
@@ -0,0 +1,7 @@
+package com.TSI.app;
+
+public class Util {
+   public static void printMessage(String message){
+	System.out.println(message);
+   }
+}
